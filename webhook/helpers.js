be061685acfc49646db3ee278bc594ff7aa8b41f
Khattak <akkhattak65@gmail.com>
2020-11-17 20:55:33 +0500

    feat: design discord embeds

--- webhook/helpers.js
+++ webhook/helpers.js
@@ -14,22 +14,88 @@ async function getGitHubProject(payload) {
   return (await octokit.projects.get({ project_id: projectId })).data;
 }
 
+async function getProjectCardCol(colId) {
+  // This will not work for Created/ Edited Etc (Try Catch is)
+  if(!colId) {
+    return;
+  }
+  return (await octokit.projects.getColumn({
+    column_id: colId,
+  }));
+}
+
 async function prepareMessage({ payload, githubProject }) {
-  const { action, changes, project_card } = payload;
-  const { html_url: githubProjectUrl } = githubProject;
+  const { action, changes, project_card, sender, repository } = payload;
+  const { login: userName, avatar_url: userAvatar, html_url: userUrl } = sender;
+  const { html_url: githubProjectUrl, name: cardName } = githubProject;
+  const { name: repoName } = repository;
+  let description = project_card.note;
+  let prevColName = { name: '-' };
+  let thumbnail = 'https://cdn.iconscout.com/icon/free/png-512/issue-4-433271.png';
+  const prevColId = changes.column_id;
+  const newColName = (await getProjectCardCol(project_card.column_id)).data;
+  // NOTE: Prev Column is only Available for Action == Moved
+  if(prevColId) {
+    prevColName = (await getProjectCardCol(changes.column_id.from)).data;
+  }
+  if(!description) {
+    description = 'No Description';
+  }
+  let color = '#ecf0f1';
 
   if (action === 'moved' && !changes) {
     // Ignore card moves within the same column
     return null;
   }
 
-  // TODO Convert payload to MessageEmbed based on action
+  switch (action) {
+  case 'created':
+    color = '#ecf0f1';
+    thumbnail = 'https://cdn.iconscout.com/icon/free/png-512/issue-4-433271.png';
+    break;
+  case 'edited':
+    color = '#3498db';
+    thumbnail = 'https://cdn.iconscout.com/icon/free/png-512/issue-4-433271.png';
+    break;
+  case 'moved':
+    color = '#2ecc71';
+    thumbnail = 'https://cdn.iconscout.com/icon/free/png-512/issue-4-433271.png';
+    break;
+  case 'converted':
+    color = '#e67e22';
+    thumbnail = 'https://cdn.iconscout.com/icon/free/png-512/issue-4-433271.png';
+    break;
+  case 'deleted':
+    color = '#e74c3c';
+    thumbnail = 'https://cdn.iconscout.com/icon/free/png-512/issue-4-433271.png';
+    break;
+  }
+
 
   const embed = new MessageEmbed()
-    .setTitle(`Project card ${payload.action}`)
+    .setTitle(`${repoName}`)
     .setURL(`${githubProjectUrl}#card-${project_card.id}`)
-    .setColor(0xff0000)
-    .setDescription(`\`\`\`${project_card.note}\n\`\`\``);
+    .setColor(color)
+    .setThumbnail(thumbnail)
+    .setAuthor(userName, userAvatar, userUrl)
+    .addFields(
+      { name: '\u200B', value: '\u200B' },
+    )
+    .addFields(
+      { name: 'Moved From', value: `${prevColName.name}`, inline: true },
+      { name: '\u200B', value: '\u200B', inline: true },
+      { name: 'Moved To', value: `${newColName.name}`, inline: true },
+    )
+    .addFields(
+      { name: 'Card Name', value: `${cardName}`, inline: true },
+      { name: '\u200B', value: '\u200B', inline: true },
+      { name: 'Action', value: `${payload.action}`, inline: true },
+    )
+    .addFields(
+      { name: '\u200B', value: '\u200B' },
+    )
+    .setDescription(`${description}`)
+    .setTimestamp();
 
   return embed;
 }
