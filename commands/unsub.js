ce1c2adeb824bcce33d18dc7e3d862ff1c6ac3be
Maaz Bin Tahir Saeed <38937684+Diaga@users.noreply.github.com>
2020-11-18 13:48:27 +0500

    feat(commands): implement discord command handlers (#12)
    Co-authored-by: Wei He <git@weispot.com>

--- commands/unsub.js
+++ commands/unsub.js
@@ -1,18 +1,27 @@
+const { remove } = require('../datastore');
+const { validateGithubProjectUrl } = require('./utils');
+
 module.exports = {
   name: 'unsub',
   aliases: ['unsubscribe'],
-  description: 'Unsubscribe to a GitHub project board',
+  description: 'Unsubscribe to a GitHub Project board',
   usage: 'https://github.com/[owner]/[repo]/projects/1',
   args: true,
   async execute(message, args) {
     const channelId = message.channel.id;
-    const githubProjectUrl = args[0];
+    const githubProjectUrl = args[0].toLowerCase();
 
-    console.log({ 'command': 'unsub', channelId, githubProjectUrl });
-    // TODO Remove channel id and githubProjectUrl from the database, send message if not found
-
-    message.channel.send(`Unsubscribed to \`${githubProjectUrl}\``);
-
-    // TODO send message if errored
+    if (validateGithubProjectUrl({ githubProjectUrl: githubProjectUrl })) {
+      try {
+        remove({ channelId: channelId, githubProjectUrl: githubProjectUrl });
+        message.channel.send(`Unsubscribed from \`${githubProjectUrl}\``);
+      }
+      catch (error) {
+        message.channel.send(`You are not subscribed to \`${githubProjectUrl}\`!`);
+      }
+    }
+    else {
+      message.channel.send('Please use correct format:\n`https://github.com/[owner/org]/[repo]/projects/[0-9]+`');
+    }
   },
 };
